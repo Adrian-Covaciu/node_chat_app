9e6fdd711d0a11470c86db9d0eea7294cebe94d3
Adrian-Covaciu <34918905+Adrian-Covaciu@users.noreply.github.com>
2021-08-20 10:01:24 +0300

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #docker configuration and installation
-sudo yum install -y yum-utils evice-mapper-persistent-data lvm2
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum -y install docker-ce
 
